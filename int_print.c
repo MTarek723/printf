88de5c3db1bda576fbb42b88bcce6b28d64dc26d
Mahmoud Tarek <mahmoud_ta_rek@outloook.com>
2023-09-18 16:25:32 -0700

    L

---
+++ int_print.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * int_print - prints integer
+ * @i: argument to print
+ * Return: number of characters printed
+ */
+int int_print(va_list in)
+{
+	int x = va_arg(in, int);
+	int num;
+	int lst = x % 10;
+	int digit;
+	int y = 1;
+	int  i = 1;
+
+	x = x / 10;
+	num = x;
+
+	if (lst < 0)
+	{
+		_putchar('-');
+		num = -num;
+		x = -x;
+		lst = -lst;
+		i++;
+	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			y = y * 10;
+			num = num / 10;
+		}
+		num = x;
+		while (y > 0)
+		{
+			digit = num / y;
+			_putchar(digit + '0');
+			num = num - (digit * y);
+			y = y / 10;
+			i++;
+		}
+	}
+
+	_putchar(lst + '0');
+
+	return (i);
+}
